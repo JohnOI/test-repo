d0e56bf35f7b4523daef940f9608ff98c9da9206
John Immanuel <john.c.immanuel1@gmail.com>
2020-12-08 12:07:04 +0100

    commit from local

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+
+public class HelloWorld {
+
+
+    public static void main (String [] args) {
+        System.out.println("Hello World.");
+    }
+
+}
+
+/*
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
+
+*/
